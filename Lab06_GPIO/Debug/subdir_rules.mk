02ad97c7208fcae193679b61de5f84e53a0689b8
Sergio Cruz <huky_sergio@yahoo.es>
2026-03-06 11:55:06 +0100

    Initial commit: Project setup

---
+++ Lab06_GPIO/Debug/subdir_rules.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+Clock.obj: /home/sergio/TI_downloads/base\ projects/tirslk_max_1_00_00/inc/Clock.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include/CMSIS" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include/CMSIS" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+Reflectance.obj: /home/sergio/TI_downloads/base\ projects/tirslk_max_1_00_00/inc/Reflectance.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include/CMSIS" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+TExaS.obj: /home/sergio/TI_downloads/base\ projects/tirslk_max_1_00_00/inc/TExaS.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include" --include_path="/opt/ti/ccs2031/ccs/ccs_base/arm/include/CMSIS" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
